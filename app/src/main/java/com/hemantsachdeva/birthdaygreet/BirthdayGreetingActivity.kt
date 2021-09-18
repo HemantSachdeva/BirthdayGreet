98bdf8a83737e2ac4491feab8a819b3dd8e599b3
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-09-18 17:15:54 +0530

    Create a new empty BirthdayGreetingActivity and a simple Toast onClick
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

---
+++ app/src/main/java/com/hemantsachdeva/birthdaygreet/BirthdayGreetingActivity.kt
@@ -0,0 +1,11 @@
+package com.hemantsachdeva.birthdaygreet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+    }
+}
--- app/src/main/java/com/hemantsachdeva/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/hemantsachdeva/birthdaygreet/MainActivity.kt
@@ -2,10 +2,16 @@ package com.hemantsachdeva.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        Toast.makeText(applicationContext, "You clicked the button", Toast.LENGTH_LONG).show()
     }
 }
